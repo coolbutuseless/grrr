63b871bba6cf3ade74b5c84aea5a62fcc6b20cc7
mike <mikefc@coolbutuseless.com>
2018-12-09 21:11:30 +1000

    add default_args_to_global_env()

---
+++ NEWS.md
@@ -0,0 +1,9 @@
+# grrr 0.1.1
+
+* Added `default_args_to_global_env()` to dump the default arguments from a 
+  function into the global namespace.
+
+# grrr 0.1.0
+
+* Initial release.
+* Functions for changing default arguments to functions, even within packages
---
+++ R/global.R
@@ -0,0 +1,23 @@
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#' Extract default arguments for a function into the global environment
+#'
+#' @inheritParams get_default_args
+#'
+#' @export
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+default_args_to_global_env <- function(func_name, package_name=NULL) {
+  default_args <- get_default_args(func_name, package_name)
+
+  for (i in seq_along(default_args)) {
+    x     <- names(default_args)[i]
+    value <- default_args[[i]]
+    if (is.call(value)) {
+      value <- eval(value, envir = .GlobalEnv)
+    }
+    message("Assigning: ", x, " <- ", deparse(value))
+    assign(x, value, .GlobalEnv)
+  }
+
+}
